f4b514fb161411f55be91d29935a6eb798746c86
Soumya323 <70372822+Soumya323@users.noreply.github.com>
2023-07-03 14:56:07 +0000

    added filteration, checkbox and state changes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,110 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+    var PRODUCTS = [
+        { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+        { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+        { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+        { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+        { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+        { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
+    ]
+
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <FilterableProductTable products={PRODUCTS} />
+            </header>
+        </div>
+    );
 }
+
+function ProductCategoryRow({ category }) {
+    return (<tr>
+        <th colSpan={2}>
+            {category}
+        </th>
+    </tr>);
+}
+
+function ProductRow({ product }) {
+    var tableRow = product.stocked ? product.name :
+        <span style={{ color: 'red' }}>{product.name}</span>
+
+    return (<tr>
+        <td>{tableRow}</td>
+        <td>{product.price}</td>
+    </tr>);
+}
+
+function ProductTable({ products, filterText, inStockOnly }) {
+
+    var lastCategory = null;
+    const rows = [];
+
+    products.map((product) => {
+
+        if (product.name.toLowerCase().indexOf(filterText.toLowerCase()) === -1) {
+            return;
+        }
+
+        if (lastCategory !== product.category) {
+            rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
+        }
+        if ((inStockOnly && product.stocked) || (!inStockOnly)) {
+            rows.push(<ProductRow product={product} key={product.name} />);
+        }
+
+        lastCategory = product.category;
+    }
+    );
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+            <tbody>
+                {rows}
+            </tbody>
+        </table>
+
+    );
+}
+
+function SearchBar({ filterText, inStockOnly, onFilterTextChange, onInStockOnlyChange }) {
+    return (<form>
+        <input type='text' value={filterText} placeholder='Search...'
+            onChange={(e) => onFilterTextChange(e.target.value)} />
+        <label>
+            <input type='checkbox' checked={inStockOnly}
+                onChange={(e) => onInStockOnlyChange(e.target.checked)} />
+            {' '}   Only show products in stock.
+        </label>
+    </form>);
+}
+
+
+function FilterableProductTable({ products }) {
+
+    const [filterText, setFilterText] = useState('');
+    const [inStockOnly, setInStockOnly] = useState(false);
+
+    return (
+        <div>
+            <SearchBar filterText={filterText} inStockOnly={inStockOnly}
+                onFilterTextChange={setFilterText} onInStockOnlyChange={setInStockOnly} />
+            <ProductTable products={products} filterText={filterText} inStockOnly={inStockOnly} />
+        </div>
+    );
+}
+
+
+
 
 export default App;
